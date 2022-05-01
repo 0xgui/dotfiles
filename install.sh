5b2f7ff644f6dd5a10ded9476a609c61d2473858
0xgui <xgui@mailbox.org>
2022-05-01 15:50:54 +0100

    Add sway DE

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ echo "Set Hostname"
 hostnamectl set-hostname linux
 
 echo "Enable RPMFusion"
-sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \ 
-	   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 echo "Install Flatpak"
 sudo dnf install -y flatpak
@@ -17,19 +16,48 @@ sudo dnf -y install  \
 	yubioath-desktop \
 	firefox \
 	fzf \
-	htop \
-	vlc  \
+	bpytop \
 	mpv \
 	thunderbird \
 	neovim \
+	util-linux \
+	sway waybar swayidle swaybg wob \
+	blueman \
+	wdisplays \
+	wofi \
+	wlogout \
+	dnf-plugins-core \
+	mako \
+	alacritty \
+	libappindicator \
+	hstr \
+	ripgrep \
+	gvfs-smb \
+  cifs-utils \
+	zathura \
+	zathura-pdf-poppler \
+	grim swappy \
+	udiskie
 
 echo "Create Folders"
-mkdir -p ~/temp ~/scripts ~/repos ~/projects ~/apps
+mkdir -p ~/temp ~/projects 
 mkdir -p ~/.local/share/nvim/backup
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/sway
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/wofi
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/mako
+mkdir -p ~/.config/zathura
 
 echo "dotfiles"
-cp .config/nvim/* ~/.config/nvim/
+cp -r .config/nvim/* ~/.config/nvim/
+cp -r .config/sway/* ~/.config/sway/
+cp -r .config/waybar/* ~/.config/waybar/
+cp -r .config/wofi/* ~/.config/wofi/
+cp -r .config/alacritty/* ~/.config/alacritty/
+cp -r .config/mako/* ~/.config/mako/
+cp -r .config/zathura/* ~/.config/zathura/
 
 echo "Install Finished. Please Reboot"
 
